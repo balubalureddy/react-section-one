cd32b4cb4eceb492c1009d1a09374bdc5eaae3d8
Balu <balayashwanth1@gmail.com>
2021-05-13 20:29:11 +0530

    pushing chsnges, add filter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,25 @@
+import React, { useState } from 'react';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import ExpensseItem from './ExpenseItems';
 import './Expenses.css'
 
 const Expenses = (props) => {
+
+    const [filteredYear, setFilteredYear] = useState('2021');
     const ExpensesItems = props.expenses.map((expense,i) => 
         <ExpensseItem key={i} title={expense.title} amount={expense.amount} date={expense.date} />)
+    
+    const handelfilterChange = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    }
     return (
-        <Card className="expenses">
-            {ExpensesItems}
-        </Card>
+        <React.Fragment>
+            <Card className="expenses">
+                <ExpensesFilter selectedYear={filteredYear} getfilterChange={handelfilterChange}/>
+                {ExpensesItems}
+            </Card>
+        </React.Fragment>
     )
 }
 export default Expenses;
