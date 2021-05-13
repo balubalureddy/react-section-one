88366e44c09576de782e862d6cd5b87fe0e00b45
Balu <balayashwanth1@gmail.com>
2021-05-13 22:40:52 +0530

    pushing changes regarding conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses"
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DummyExpense = [
+  {id: 'e1', title: 'Car Insurence', amount: 350, date:new Date(2021,2,28)},
+  {id: 'e2',title: 'Car Insurence', amount: 350, date:new Date(2023,2,28)},
+  {id: 'e3',title: 'Car Insurence', amount: 350, date:new Date(2022,2,28)}
+]
 const App = () => {
-  const expenses = [
-    {id: 'e1', title: 'Car Insurence', amount: 350, date:new Date(2021,2,28)},
-    {id: 'e2',title: 'Car Insurence', amount: 350, date:new Date(2021,2,28)},
-    {id: 'e3',title: 'Car Insurence', amount: 350, date:new Date(2021,2,28)}
-  ]
+
+  const [expenses, setExpenses] = useState(DummyExpense)
   const handelAddExpenseData = (ExpenseData) => {
-    
+    setExpenses(prevExpenses => {
+      return [ExpenseData, ...prevExpenses]
+    })
   }
   return (
     <div>
